dbeeb63cbe9972f9ca41c493fd2262d767c3b66d
Muthu <muthunarayananc@laptop.com>
2025-08-22 09:13:15 -0400

    Fix ReactNativeHost method name: change getReactPackages() to getPackages()

--- android/app/src/main/java/com/jamvisrn/MainApplication.java
+++ android/app/src/main/java/com/jamvisrn/MainApplication.java
@@ -27,7 +27,7 @@ public class MainApplication extends MultiDexApplication implements ReactApplica
         }
 
         @Override
-        protected List<ReactPackage> getReactPackages() {
+        protected List<ReactPackage> getPackages() {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
