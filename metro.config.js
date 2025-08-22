4a79bde8a25497c609dded96b3ee083ab4a1aee7
Muthu <muthunarayananc@laptop.com>
2025-08-22 14:29:20 -0400

    Fix minification issue: Add NODE_ENV=development to disable minification in EAS builds

--- metro.config.js
+++ metro.config.js
@@ -1,10 +1,11 @@
-const { getDefaultConfig } = require('expo/metro-config');
-const { mergeConfig } = require('@react-native/metro-config');
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
 /**
- * Metro configuration for maximum backward compatibility
- * https://facebook.github.io/metro/docs/configuration
+ * Metro configuration
+ * https://reactnative.dev/docs/metro
  *
  * @type {import('metro-config').MetroConfig}
  */
-module.exports = getDefaultConfig(__dirname);
+const config = {};
+
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
