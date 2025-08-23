5015c85ce3b539fffe95e0dcdd8c4b419e180c6a
Muthu <muthunarayananc@laptop.com>
2025-08-22 21:48:06 -0400

    Nuclear ES5 approach - no modern JS syntax to trigger export errors

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App.minimal';
-import {name as appName} from './app.json';
+var AppRegistry = require('react-native').AppRegistry;
+var App = require('./App.es5');
+var appName = require('./app.json').name;
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, function() { return App; });
--- metro.config.js
+++ metro.config.js
@@ -1,24 +1,25 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
 /**
- * Metro configuration with minification DISABLED
+ * Metro configuration with minification DISABLED and ES5 output
  * https://reactnative.dev/docs/metro
  *
  * @type {import('metro-config').MetroConfig}
  */
 const config = {
   transformer: {
-    // Completely disable minification
+    // Completely disable minification and force ES5
     minifierPath: require.resolve('metro-minify-terser'),
     minifierConfig: {
-      ecma: 8,
+      ecma: 5, // Force ES5 output to avoid export issues
       keep_fnames: true,
       mangle: false,
       compress: false,
       output: {
-        ascii_only: false,
+        ascii_only: true,
         comments: true,
         beautify: true,
+        ecma: 5,
       },
     },
   },
