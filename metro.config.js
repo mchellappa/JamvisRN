fa7bfdb144bb87b6c0b3aba91bf1e6ef81fc62e3
Muthu <muthunarayananc@laptop.com>
2025-08-22 21:08:50 -0400

    COMPREHENSIVE MINIFICATION FIX: Disable minification in Metro config + multiple env vars

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,27 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
 /**
- * Metro configuration
+ * Metro configuration with minification DISABLED
  * https://reactnative.dev/docs/metro
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  transformer: {
+    // Completely disable minification
+    minifierPath: require.resolve('metro-minify-terser'),
+    minifierConfig: {
+      ecma: 8,
+      keep_fnames: true,
+      mangle: false,
+      compress: false,
+      output: {
+        ascii_only: false,
+        comments: true,
+        beautify: true,
+      },
+    },
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
